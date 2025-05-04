cbaaba204d2f2ebe4d33f8132cd562ee82f9b966
Admin <admin@MacBook-Air-de-Joel.local>
2025-05-03 19:37:02 -0600

    Primer respaldo del sitio Neox Solutions

---
+++ soluciones.js
@@ -0,0 +1,16 @@
+// Animación al hacer scroll
+const secciones = document.querySelectorAll('.seccion');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+secciones.forEach(seccion => {
+  observer.observe(seccion);
+});
